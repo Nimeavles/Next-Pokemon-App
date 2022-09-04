6dc57f5bfbee3b9a751ea9d708dc69d59a2b9e5b
Nimeavles <prlf505@gmail.com>
2022-09-04 23:29:19 +0200

    Implement favorite pokemons

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,16 +1,46 @@
-import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+import { useState } from "react";
+
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+
+import confetti from 'canvas-confetti';
+
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
-import { Pokemon, PokemonListResponse, SmallPokemon } from "../../interfaces";
+import { Pokemon } from "../../interfaces";
+import { localFavorites } from "../../utils";
 
 interface Props {
   pokemon: Pokemon;
 }
 
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+  const [isInFavorites, setIsInFavorites] = useState(
+    localFavorites.existInFavorites(pokemon.id)
+  );
+
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id);
+    setIsInFavorites( !isInFavorites );
+
+    if( !isInFavorites ) {
+      confetti({
+        zIndex: 999,
+        particleCount: 100,
+        spread: 160,
+        angle: -160,
+        origin: {
+          x: 1,
+          y: 0
+        }
+      })
+    }
+  };
+
   return (
-    <Layout title={pokemon.name}>
+    <Layout
+      title={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+    >
       <Grid.Container css={{ marginTop: "5px" }} gap={2}>
         <Grid xs={12} sm={4}>
           <Card hoverable css={{ padding: "30px" }}>
@@ -37,14 +67,17 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
               <Text h1 transform="capitalize">
                 {pokemon.name}
               </Text>
-              <Button color="gradient" ghost>
-                Guardar en favoritos
+              <Button color="gradient"
+                ghost={ !isInFavorites } 
+                onClick={onToggleFavorite}
+              >
+                { isInFavorites ?  'En favoritos' : 'Guardar en favoritos'}
               </Button>
             </Card.Header>
 
             <Card.Body>
               <Text size={30}>Sprites: </Text>
-              <Container direction='row' display="flex">
+              <Container direction="row" display="flex">
                 <Image
                   src={pokemon.sprites.front_default}
                   alt={pokemon.name}
